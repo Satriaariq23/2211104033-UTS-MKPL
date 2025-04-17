bcc7f34750194c254fcefd28cf3510652cca2936
Satriaariq23 <136548620+Satriaariq23@users.noreply.github.com>
2025-04-17 21:24:03 +0700

    Menambahkan enum Gender di dalam class Employee
    Mengubah tipe data gender dari boolean menjadi Gender

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -6,7 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
-
+	public enum Gender {
+        MALE, FEMALE
+	}	
 	private String employeeId;
 	private String firstName;
 	private String lastName;
@@ -19,7 +21,7 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private Gender gender;
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -30,6 +32,7 @@ public class Employee {
 
 	private List<String> childNames;
 	private List<String> childIdNumbers;
+	private boolean Gender;
 	
 	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
@@ -41,7 +44,7 @@ public class Employee {
 		this.monthJoined = monthJoined;
 		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
-		this.gender = gender;
+		this.Gender = gender;
 		
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
